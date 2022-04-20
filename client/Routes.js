4f98562d8185ac32d656730aa320e7c4d0663f7c
Andrey Statkevskiy <andstatik@gmail.com>
2022-04-20 18:09:12 -0400

    adjusted the seed file

--- client/Routes.js
+++ client/Routes.js
@@ -24,6 +24,7 @@ class Routes extends Component {
         {isLoggedIn ? (
           <Switch>
             <Route path="/home" component={Home} />
+            <Route path="/products" exact component={AllProducts} />
             <Redirect to="/home" />
           </Switch>
         ) : (
